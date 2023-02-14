fedce26da26df175fbf877c8e264b87658657e40
joulongleu <6103334+joulongleu@users.noreply.github.com>
2023-02-14 18:26:40 +0800

    Update index.py

--- index.py
+++ index.py
@@ -44,7 +44,7 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_msg))
-  
+    return
            
 
 if __name__ == "__main__":
