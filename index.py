13872ba7f8538c92142ea2f0b2600b31be92cfeb
joulongleu <6103334+joulongleu@users.noreply.github.com>
2023-02-14 18:16:01 +0800

    Update index.py

--- index.py
+++ index.py
@@ -36,20 +36,15 @@ def callback():
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     global working_status
-    working_status = True
+    working_status = True 
     
-    if event.message.type != "text":
-        return
-    
-
-    if working_status:
-        chatgpt.add_msg(f"Human:{event.message.text}?\n")
-        reply_msg = chatgpt.get_response().replace("AI:", "", 1)
-        chatgpt.add_msg(f"AI:{reply_msg}\n")
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=reply_msg))
-        return
+    chatgpt.add_msg(f"Human:{event.message.text}?\n")
+    reply_msg = chatgpt.get_response().replace("AI:", "", 1)
+    chatgpt.add_msg(f"AI:{reply_msg}\n")
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_msg))
+  
            
 
 if __name__ == "__main__":
